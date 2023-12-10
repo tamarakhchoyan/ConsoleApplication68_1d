d32bc5fee7dedbd04ab43b0ee5e6db01559a2010
User <User@DESKTOP-9TGTEUB>
2023-12-10 15:24:22 +0400

    Add project files.

---
+++ ConsoleApplication68_1d/ConsoleApplication68_1d.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+int main()
+{
+	const int n = 9;
+	int i;
+	int a[n];
+	int mul = 1;
+	for (i = 0; i < n; ++i)
+		std::cin >> a[i];
+	for(i=0;i<n;++i)
+		if(a[i]<15 && a[i]%3==0)
+		{
+			mul *= a[i];
+			std::cout << mul << std::endl;
+		}
+	return 0;
+}
